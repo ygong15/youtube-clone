d4a9f562fc5341d3c46fbd57bb03abf8d51aefb4
Yuchen (Aaron) Gong <gycaaron@outlook.com>
2023-10-06 23:56:06 -0700

    process videos locally done

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,35 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
+app.post("/process-video", (req, res) => {
+    // get path of the input video file from the request body
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if(!inputFilePath)
+    {
+        return res.status(400).send("Bad Request: Missing input file path.");
+    }
+    if(!outputFilePath)
+    {
+        return res.status(400).send("Bad Request: Missing input file path.");
+    }
+    ffmpeg(inputFilePath)
+        .outputOption("-vf", "scale=-1:360")
+        .on("end", () => {
+            res.status(200).send("Processing finished successfully.")
+        })
+        .on("error", (err) => {
+            console.log(`An error occured: ${err.message}`);
+            res.status(500).send(`Internal Server Error: ${err.message}`);
+        })
+        .save(outputFilePath);
 });
 
-app.listen()
-
-// something something
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Video processing service listening at http://localhost:${port}`);
+});
